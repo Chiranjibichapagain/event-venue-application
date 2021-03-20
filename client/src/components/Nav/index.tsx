0bc5497f28296bfa4185d94b85520398e9691ec4
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-03-20 14:11:57 +0200

    [Feat] nav bar with google login

--- client/src/components/Nav/index.tsx
+++ client/src/components/Nav/index.tsx
@@ -1,11 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { useHistory } from 'react-router-dom';
+
+import { UserData } from '../../types';
 
 import logo from '../../Assets/logo2.svg';
+
 import './Nav.scss';
+
 const Nav = () => {
+  const history = useHistory();
+  const [log, setLog] = useState<boolean>(false);
+  const [userData, setUserData] = useState<UserData | null>();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleLoginSuccess = (response: any) => {
+    const data = response.profileObj;
+    const stringData = JSON.stringify(data);
+    localStorage.setItem('booking-user-info', stringData);
+    setLog(true);
+  };
+
+  useEffect(() => {
+    const localData = localStorage.getItem('booking-user-info');
+    const parsedData = localData && JSON.parse(localData);
+    setUserData(parsedData);
+  }, [log]);
+
+  const handleLoginFail = () => {
+    console.log('failed');
+  };
+
+  const handleLogout = () => {
+    setUserData(null);
+    localStorage.removeItem('quiz-user-info');
+    setLog(false);
+  };
+  const toHome = () => {
+    history.push('./');
+  };
+
+  const googleClient = '1022731832769-0epv227hsfh2rpvsgroeg558uttkhg4b.apps.googleusercontent.com';
   return (
     <div className="nav">
-      <img className="nav__logo" src={logo} alt="logo" />
+      <img onClick={toHome} className="nav__logo" src={logo} alt="logo" />
+      {!userData ? (
+        <GoogleLogin
+          className="nav__login"
+          clientId={googleClient}
+          buttonText="Login"
+          onSuccess={handleLoginSuccess}
+          onFailure={handleLoginFail}
+          cookiePolicy={'single_host_origin'}
+        />
+      ) : (
+        <div className="nav__log-div">
+          <GoogleLogout
+            clientId={googleClient}
+            buttonText="Log Out"
+            onLogoutSuccess={handleLogout}
+          />
+          <div className="nav__profile-div">
+            <img className="nav__profile-img" src={userData.imageUrl} alt={userData.imageUrl} />
+            <p className="nav__profile-name">{` Hi ${userData.givenName}`}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- client/src/utils/ProtectedRoute.tsx
+++ client/src/utils/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Route, Redirect, useLocation } from 'react-router-dom';
 
 function ProtectedRoute({ component: Component, ...rest }) {
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const location = useLocation();
 
   useEffect(() => {
