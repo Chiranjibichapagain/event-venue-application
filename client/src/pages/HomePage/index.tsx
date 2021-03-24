f407d29f8d174aa8b7449f17fb1900ed46ef0576
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-03-24 14:29:29 +0200

    [Feat] Filter try

--- client/src/pages/HomePage/index.tsx
+++ client/src/pages/HomePage/index.tsx
@@ -4,50 +4,21 @@ import Filters from '../../components/Filters';
 import VenueTile from '../../components/VenueTile';
 import { Venue } from '../../types';
 import { data } from '../../utils/dummydata';
+import { useFilter } from '../../Hooks/useFilter';
 
 import './HomePage.scss';
 
 const HomePage = () => {
   const [price, setPrice] = useState('');
   const [size, setSize] = useState('');
-  const [venues, setVenues] = useState<Venue[]>(data);
 
-  useEffect(() => {
-    handleFilter();
-    console.log(
-      'venuee--',
-      venues.map((v) => v.price)
-    );
-  }, [price, size, venues]);
-
-  const handleFilter = () => {
-    const priceFilter = () => {
-      if (!price) {
-        return data;
-      } else if (price === 'cheap') {
-        return data.sort((a, b) => a.price - b.price);
-      } else {
-        return data.sort((a, b) => b.price - a.price);
-      }
-    };
-    const sizeFilter = () => {
-      if (!size) {
-        return priceFilter();
-      } else if (size === 'small') {
-        return priceFilter().sort((a, b) => a.people - b.people);
-      } else {
-        return priceFilter().sort((a, b) => b.people - a.people);
-      }
-    };
-
-    setVenues(sizeFilter());
-  };
+  const [venue] = useFilter(price, size);
 
   return (
     <div className="home">
       <Filters setPrice={setPrice} setSize={setSize} />
       <div className="home__venues">
-        {venues && venues.map((venue: Venue) => <VenueTile key={venue.id} data={venue} />)}
+        {venue && venue.map((venue: Venue) => <VenueTile key={venue.id} data={venue} />)}
       </div>
     </div>
   );
