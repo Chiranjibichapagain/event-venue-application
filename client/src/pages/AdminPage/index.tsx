b698afec06f70a05d345fb3d861277b1040a3457
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-03-30 18:33:22 +0300

    [Feat] Admin page aside nav

--- client/src/pages/AdminPage/index.tsx
+++ client/src/pages/AdminPage/index.tsx
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaCalendar, FaIgloo, FaPlusCircle } from 'react-icons/fa';
 
 import './AdminPage.scss';
 
 function AdminPage() {
-  return <div>This is admin page</div>;
+  const [view, setView] = useState('bookings');
+  return (
+    <div className="admin">
+      <div className="admin__aside">
+        <div
+          onClick={() => setView('bookings')}
+          className={
+            view === 'bookings' ? 'admin__nav-items admin__nav-items--active' : 'admin__nav-items'
+          }
+        >
+          {' '}
+          <FaCalendar className="admin__icon" /> <span className="admin__nav-text">Bookings</span>
+        </div>
+        <div
+          onClick={() => setView('venues')}
+          className={
+            view === 'venues' ? 'admin__nav-items admin__nav-items--active' : 'admin__nav-items'
+          }
+        >
+          {' '}
+          <FaIgloo className="admin__icon" /> <span className="admin__nav-text">Venues</span>
+        </div>
+        <div
+          onClick={() => setView('new-venue')}
+          className={
+            view === 'new-venue' ? 'admin__nav-items admin__nav-items--active' : 'admin__nav-items'
+          }
+        >
+          {' '}
+          <FaPlusCircle className="admin__icon" /> <span className="admin__nav-text">Add New</span>
+        </div>
+      </div>
+
+      <div className="admin__main">
+        <h1>Admin Dashboard</h1>
+        {view === 'bookings' && (
+          <div>
+            <h1>Calender</h1>
+          </div>
+        )}
+        {view === 'venues' && (
+          <div>
+            <h1>Venue List</h1>
+          </div>
+        )}
+        {view === 'new-venue' && (
+          <div>
+            <h1>Create New venue</h1>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default AdminPage;
