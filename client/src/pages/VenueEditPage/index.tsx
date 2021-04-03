37ed93b9fb5b631b71f780884de7fc9597c35d02
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-04-03 13:46:08 +0300

    [Feat] Textarea

--- client/src/pages/VenueEditPage/index.tsx
+++ client/src/pages/VenueEditPage/index.tsx
@@ -11,15 +11,25 @@ const VenueEditPage = ({ match }) => {
   const venue = data && data.find((item) => item.id === parseInt(id));
 
   const [fields, setFields] = useForm({
-    name: venue?.venueName
+    name: venue?.venueName,
+    price: venue?.price,
+    area: venue?.area,
+    address: venue?.address,
+    people: venue?.people,
+    desription: venue?.description
   });
 
-  const { name } = fields;
+  const { name, price, address, people, area, description } = fields;
 
   return (
     <div className="venue-edit">
       <h1 className="venue-edit__heading">Update Venue Information</h1>
       <div className="venue-edit__form">
+        <Input id="name" value={name} handleInputChange={setFields} type="text" />
+        <Input id="address" value={address} handleInputChange={setFields} type="text" />
+        <Input id="area" value={area} handleInputChange={setFields} type="text" />
+        <Input id="people" value={people} handleInputChange={setFields} type="number" />
+        <Input id="price" value={price} handleInputChange={setFields} type="number" />
         <Input id="name" value={name} handleInputChange={setFields} type="text" />
       </div>
     </div>
