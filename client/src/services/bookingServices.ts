d932432a0f145d3eac2b81a9130200694b0c4f02
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-05-08 23:18:03 +0300

    [Fix] heroku error

--- client/src/services/bookingServices.ts
+++ client/src/services/bookingServices.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:5000/api/booking';
+const baseUrl = 'api/booking';
 
 export const makeBooking = async (bookingData) => {
   return await axios.post(baseUrl, bookingData);
