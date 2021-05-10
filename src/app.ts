4b5d54f567cc0dc27cae2bd93d363dc5c8360c23
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-05-10 16:21:44 +0300

    [Fix] heroku

--- src/app.ts
+++ src/app.ts
@@ -32,9 +32,18 @@ app.use('/api/booking', bookingRouter);
 app.use('/api/admin', adminRouter);
 
 //Heroku---
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+
+//   app.get('*', (request: Request, response: Response) => {
+//     response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//   });
+// }
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/')));
-  app.get('/*', function (req: Request, res: Response) {
+
+  app.get('/*', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, 'client/index.html'));
   });
 }
